8fc617ba3674db155eb19e7b80b1c8dd84ab3941
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-11-13 15:55:33 -0500

    seed_change

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,15 @@ Job.destroy_all
 Task.destroy_all
 UserJob.destroy_all
 
-# sara = User.create(name:"Sara",gender:"F")
-med_school = Job.create(name: 'med_school', happiness_increment: 4, level: 1, money: 2000)
-construction_helper = Job.create(name: 'Helper', happiness_increment: 5, level: 1, money: 3500 )
-internship = Job.create(name: 'Internship', happiness_increment: 2, level: 1, money: 2500 )
+
 seminary = Job.create(name: 'Seminary', happiness_increment: 8, level: 1, money: 0)
+construction_helper = Job.create(name: 'Helper', happiness_increment: 5, level: 1, money: 3500 )
+
+
+
+
+internship = Job.create(name: 'Internship', happiness_increment: 2, level: 1, money: 2500 )
+med_school = Job.create(name: 'med_school', happiness_increment: 4, level: 1, money: 2000)
 doctor = Job.create(name: 'doctor', happiness_increment: 6, level: 2, money: 100000)
 med_school.tasks << Task.create(description:"I am a fun task!",happiness_increment: -2, money: 10000)
 med_school.tasks << Task.create(description:"I am a doctor task!", outcome_job_id: doctor.id)
